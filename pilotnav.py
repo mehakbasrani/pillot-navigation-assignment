78651cccb36b60e2cc116608ce5fbd991550292c
mehakbasrani <55041907+mehakbasrani@users.noreply.github.com>
2021-03-26 17:19:16 +0530

    1st Assignment

---
+++ pilotnav.py
@@ -0,0 +1,9 @@
+print("Hiee there!! I am here to help you land the plan!")
+print("Just enter the altitude where your plane is flying and i will let you know is it safe for landing or not")
+altitude= int(input())
+if altitude ==3000:
+       print("Hurray!!You are already to land!")
+elif altitude>3000 and altitude<6000:
+     print("you can ty to land")
+else:   
+     print("alert!! you cannot land")  
